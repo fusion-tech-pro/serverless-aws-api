0a3b1c07e1acbc5df126c402e8437928dd90bb88
Maxim Konovodov <maxim.konovodov@fusion-tech.pro>
2021-04-12 19:27:30 +0300

    Implement currency filter

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 export const createFilter = (params: any) => {
-  const options: any = {};
+  const options: any = {
+    where: {}
+  };
 
   options.limit = params.perPage || 5;
   let page;
@@ -14,10 +16,15 @@ export const createFilter = (params: any) => {
   const sortType = params.sortType || 'DESC';
   options.order = [[orderBy, sortType]];
 
+  if (params.currency) {
+    const currencyFilter = JSON.parse(params.currency);
+    options.where.currency = currencyFilter.length > 0
+      ? currencyFilter
+      : ["USD", "EUR"]
+  }
+
   if (params.clientId) {
-    options.where = {
-      clientId: params.clientId
-    }
+    options.where.clientId = params.clientId
   }
 
   return options;
